29a9e9acd6f7d4225330a02e5c16123871fe0be8
Bryce Motte <bryce.motte@gmail.com>
2019-12-27 16:55:25 -0800

    Update Up-down.py

--- Up-down.py
+++ Up-down.py
@@ -14,34 +14,38 @@ from multiping import MultiPing  # for ping process
 # Send ping to address
 # Check each response with receive 
 ping_EP ={'1.1.1.1': 'Cloudflare','8.8.8.8': 'Google','75.75.75.75': 'Comcast'}
-
-def get_ping(addrs):    
-    mp = MultiPing(addrs)
+addrs = ["1.1.1.1","8.8.8.8","75.75.75.75"]
+def get_ping(x):    
+    mp = MultiPing(x)
     mp.send()
-    responses, no_responses = mp.receive(0.1)
+    responses, no_responses = mp.receive(1)
+    return responses, no_responses
 
 
 
 
 if __name__ == "__main__":
-    addrs = ["1.1.1.1","8.8.8.8","75.75.75.75"]
-    pings = get_ping(addrs)
-
+    
     cw_client = boto3.client('cloudwatch', region_name='Oregon')
-
     date_now = datetime.utcnow()
-
     metric_data = []
 
-    for ping_address in pings.keys():
+    pings = get_ping(addrs)
+    
+    print(pings)
+    """
+    for ping in ping_EP.keys:
         metric_data.append({
-            'MetricName': pings_EP[ping_address] + 'Latency',
+            'MetricName': ping_EP[ping_address] + ' Latency',
             'Timestamp': date_now,
-            'Value': pings[ping_address],
+            'Value': ping[],
             'Unit': 'Seconds'
         })
+    
     print(metric_data)
+    """
+    """
     cw_client.put_metric_data(
         Namespace='IspCheck',
         MetricData=metric_data
-    )
+    """
